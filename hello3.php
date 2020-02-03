c2f2af23d8ffd8df874e13711f370bca3b154d90
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-02-03 00:12:42 -0700

    Add files via upload

---
+++ hello3.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello, World!</title>
+	<style type="text/css">
+	.bold {
+		font-weight: bolder;
+	}
+	</style>
+</head>
+<body>
+<p> The following was created by PHP:
+<?php
+/*
+ * Filename: hello3.php
+ * Book Reference: Script 1.5
+ * Created By: Larry Ullman
+*/ 
+//print "<span class=\"bold\">Hello, world!</span>";
+?>
+<!-- This is an HTML comment. -->
+</p>
+</body>
+</html>
