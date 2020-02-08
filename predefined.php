7329ca375e25b41ab7aa42c90150c65815f52e2e
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-02-08 01:23:26 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Predefined Variables</title>
+</head>
+<body>
+<pre>
+<?php // Script 2.1 - predefined.php
+print_r($_SERVER);
+?>
+</pre>
+</body>
+</html>
