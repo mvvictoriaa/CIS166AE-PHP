c806effe02f0ae8b6a795f2640abf626c4e726be
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-02-22 19:04:20 -0700

    Add files via upload

---
+++ incrementdecrement.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Increment | Decrement</title>
+</head>
+
+<body>
+<?php 
+
+$test = 1;
+echo $test++ ."<br>";     
+echo $test++ ."<b>&nbsp;increment</b>" ."<br>";
+echo $test++ ."<b>&nbsp;increment</b>" ."<br>";
+echo $test-- ."<b>&nbsp;increment</b>" ."<br>";
+echo $test-- ."<b>&nbsp;decrement</b>" ."<br>";
+echo $test ."<b>&nbsp;decrement</b>" ;
+
+
+?>
+</body>
+</html>
