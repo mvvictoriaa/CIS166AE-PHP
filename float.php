98aee3fa90f79891566baddac7bc5bfd427c0201
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-02-25 16:01:22 -0700

    Add files via upload

---
+++ float.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Float | Integers</title>
+</head>
+
+<body>
+<?php 
+
+$a = 2.763;
+$b = 8.9e1;
+$c = 4.8E-1;
+
+print "<p><b>Examples of Float:</b></p>";
+echo $a ."<br>";
+echo "<p>float = 2.763</p>";  
+echo $b ."<br>";
+echo "<p>float = 8.9e1</p>";  
+echo $c ."<br>";
+echo "<p>float = 4.8E-1</p>" ."<br>";  
+
+$d = 999;
+$e = 1;
+$f = -23456;
+
+print "<p><b>Examples of Integers:</b></p>";
+echo $d ."<br>";
+echo "<p>int = 999</p>";  
+echo $e ."<br>";
+echo "<p>int = 1</p>";  
+echo $f ."<br>";
+echo "<p>int = -23456</p>";  
+
+?>
+</body>
+</html>
