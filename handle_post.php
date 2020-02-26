644e0f9cb78451c867b3142f8bfdbaf903d8394e
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-02-25 23:36:47 -0700

    Add files via upload

---
+++ handle_post.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Forum Posting</title>
+</head>
+<body>
+<?php // Script 5.2 - handle_post.php
+
+error_reporting(E_ALL); // display all errors
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$posting = $_POST['posting'];
+
+$name = $first_name .' ' .$last_name;
+
+print "<div>Thank you, $name, for your posting: 
+<p>$posting</p></div>";
+
+?>
+</body>
+</html>
