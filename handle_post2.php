4491c2402f0144fcff05a6c76bbd814b51d2d48b
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-03-01 10:42:46 -0700

    Add files via upload

--- handle_post2.php
+++ handle_post2.php
@@ -4,18 +4,19 @@
 	<meta charset="utf-8">
 	<title>Forum Posting</title>
 </head>
+
 <body>
 <?php // Script 5.5 - handle_post2.php
 
-error_reporting(E_ALL); // display all errors
+error_reporting(0); 
 
 // Get values from the _POST array:
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
-$posting = nl2br($_POST['posting'], false);
+$posting = nl2br($_POST['posting']);
 
 // Full name variable:
-$name = $first_name .' ' .$last_name;
+$name = $first_name . ' ' . $last_name;
 
 print "<div>Thank you, $name, for your posting: 
 <p>$posting</p></div>";
@@ -24,7 +25,6 @@ print "<div>Thank you, $name, for your posting:
 $name = urlencode($name);
 $email = urlencode($_POST['email']);
 print "<p>Click <a href=\"thanks.php?name=$name&email=$email\">here</a> to continue.</p>";
-
 
 ?>
 </body>
