1dceb97a046f2b8fed9c94c4ef55b7896d3ffba7
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-03-30 21:51:17 -0700

    Add files via upload

---
+++ reset.php
@@ -0,0 +1,16 @@
+<?php // Script 9.4 - reset.php
+
+// Delete the cookies:
+setcookie('font_size', '', time() - 600, '/');
+setcookie('font_color', '', time() - 600, '/');
+
+?><!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Reset Your Settings</title>
+</head>
+<body>
+<p>Your settings have been reset! Feel free to <a href="view_settings.php">customize</a> them again.</p>
+</body>
+</html>
