774e46422d50e2c49403a42541de194d44f567fb
mvvictoriaa <60543039+mvvictoriaa@users.noreply.github.com>
2020-04-26 15:01:16 -0700

    Add files via upload

---
+++ mysqli_connect2.php
@@ -0,0 +1,8 @@
+<?php // Script 13.1 - mysqli_connect2.php
+/* This script connects to the database and establishes the character set for communications. */
+
+// Connect:
+$dbc = mysqli_connect('localhost', 'username', 'password', 'myquotes');
+
+// Set the character set:
+mysqli_set_charset($dbc, 'utf8');
